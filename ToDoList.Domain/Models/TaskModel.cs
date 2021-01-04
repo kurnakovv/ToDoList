db00607fc070112df4caf9f9152aa9a791abc556
KurnakovMaksim <maksk210@gmail.com>
2021-01-04 20:28:28 +0900

    Add task model

---
+++ ToDoList.Domain/Models/TaskModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ToDoList.Domain.Models
+{
+    public class TaskModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Completeness { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public TaskModel()
+        {
+            Id = Guid.NewGuid().ToString();
+            DateTime = DateTime.Now;
+        }
+
+        public static TaskModel GetCloneTask(TaskModel task)
+        {
+            if (task is null)
+            {
+                throw new ArgumentNullException(nameof(task));
+            }
+
+            return new TaskModel()
+            {
+                Name = task.Name,
+                Description = task.Description,
+                Completeness = task.Completeness,
+                DateTime = task.DateTime,
+            };
+        }
+    }
+}
