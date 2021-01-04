b7115abb576191054452759feb193fa2aa298d58
KurnakovMaksim <maksk210@gmail.com>
2021-01-04 20:36:56 +0900

    Implement simple CRUD system to UI

--- ToDoList.UI/Form1.Designer.cs
+++ ToDoList.UI/Form1.Designer.cs
@@ -28,13 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.EditBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddBtn
+            // 
+            this.AddBtn.Location = new System.Drawing.Point(13, 53);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(75, 23);
+            this.AddBtn.TabIndex = 0;
+            this.AddBtn.Text = "Add task";
+            this.AddBtn.UseVisualStyleBackColor = true;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
+            // 
+            // EditBtn
+            // 
+            this.EditBtn.Location = new System.Drawing.Point(3, 330);
+            this.EditBtn.Name = "EditBtn";
+            this.EditBtn.Size = new System.Drawing.Size(75, 23);
+            this.EditBtn.TabIndex = 1;
+            this.EditBtn.Text = "Edit task";
+            this.EditBtn.UseVisualStyleBackColor = true;
+            this.EditBtn.Click += new System.EventHandler(this.EditBtn_Click);
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.Location = new System.Drawing.Point(248, 330);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.DeleteBtn.TabIndex = 2;
+            this.DeleteBtn.Text = "Delete task";
+            this.DeleteBtn.UseVisualStyleBackColor = true;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.DeleteBtn);
+            this.panel1.Controls.Add(this.EditBtn);
+            this.panel1.Location = new System.Drawing.Point(462, 82);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(326, 356);
+            this.panel1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 82);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 353);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(4, 92);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(319, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(7, 150);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(316, 174);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Date time";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Description";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Date time";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Description";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Completness";
+            this.Column4.Name = "Column4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.AddBtn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Button EditBtn;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn Column4;
     }
 }
 
--- ToDoList.UI/Form1.cs
+++ ToDoList.UI/Form1.cs
@@ -1,20 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using ToDoList.Domain.Models;
+using ToDoList.Domain.Services;
+using ToDoList.Domain.Services.Abstract;
 
 namespace ToDoList.UI
 {
     public partial class Form1 : Form
     {
+        private readonly BindingSource _bindingSourceTasks = new BindingSource();
+        private readonly BindingSource _bindingSourceCurrentTask = new BindingSource();
+        private readonly ITaskService _taskService = new TaskService();
+        
         public Form1()
         {
             InitializeComponent();
+
+            SetBindings();
+            Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.MouseClick += (s, a) => SetCurrentTask();
+            LoadTasks();
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            _bindingSourceTasks.MoveLast();
+            _bindingSourceTasks.Add(new TaskModel());
+            _bindingSourceTasks.MoveNext();
+            SetCurrentTask();
+            textBox1.Focus();
+        }
+
+        private void EditBtn_Click(object sender, EventArgs e)
+        {
+            TaskModel currentTask = _bindingSourceCurrentTask.Current as TaskModel;
+            TaskModel currentTasks = _bindingSourceTasks.Current as TaskModel;
+
+            if (dataGridView1.CurrentCell.Value == null)
+            {
+                _taskService.AddTask(currentTask);
+            }
+            else
+            {
+                //_taskService.UpdateTask(currentTasks);
+                _taskService.DeleteTaskById(currentTasks.Id);
+                _taskService.AddTask(currentTask);
+                // TODO: Edited exception when we use method UpdateTask
+            }
+
+            LoadTasks();
+        }
+
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            TaskModel currentTask = _bindingSourceTasks.Current as TaskModel;
+
+            if (currentTask != null)
+            {
+                if (currentTask.Id != null)
+                {
+                    _taskService.DeleteTaskById(currentTask.Id);
+                    _bindingSourceTasks.Remove(currentTask);
+                }
+            }
+        }
+
+        private void LoadTasks()
+        {
+            IEnumerable<TaskModel> tasks = _taskService.GetAllTasks();
+            _bindingSourceTasks.DataSource = tasks;
+            SetCurrentTask();
+        }
+
+        private void SetCurrentTask()
+        {
+            if (_bindingSourceTasks.Count > 0)
+            {
+                _bindingSourceCurrentTask.List[0] = TaskModel.GetCloneTask((TaskModel)_bindingSourceTasks.Current);
+            }
+            else
+            {
+                _bindingSourceCurrentTask.List[0] = new TaskModel();
+            }
+            _bindingSourceCurrentTask.ResetItem(0);
+        }
+
+        private void SetBindings()
+        {
+            _bindingSourceTasks.DataSource = typeof(IEnumerable<TaskModel>);
+
+            dataGridView1.AutoGenerateColumns = false;
+            dataGridView1.DataSource = _bindingSourceTasks;
+
+            Column1.DataPropertyName = nameof(TaskModel.Name);
+            Column2.DataPropertyName = nameof(TaskModel.Description);
+            Column3.DataPropertyName = nameof(TaskModel.DateTime);
+
+            _bindingSourceCurrentTask.DataSource = new List<TaskModel> { new TaskModel() };
+
+            textBox1.DataBindings.Add("Text", _bindingSourceCurrentTask, nameof(TaskModel.Name));
+            textBox2.DataBindings.Add("Text", _bindingSourceCurrentTask, nameof(TaskModel.DateTime));
+            textBox3.DataBindings.Add("Text", _bindingSourceCurrentTask, nameof(TaskModel.Description));
         }
     }
 }
